e6f16ab68429495f75ecdc741f851d8e2646d26c
Admin_KamatiM <Admin_KamatiM@NAMWHKITONB23>
2024-12-28 00:35:30 +0200

    Add project files.

---
+++ Handlers/DeleteStudentHandler.cs
@@ -0,0 +1,25 @@
+﻿using CQRSAndMediatRDemo.Commands;
+using CQRSAndMediatRDemo.Repositories;
+using MediatR;
+
+namespace CQRSAndMediatRDemo.Handlers
+{
+    public class DeleteStudentHandler : IRequestHandler<CQRSAndMediatRDemo.Commands.DeleteStudentCommand, int>
+    {
+        private readonly IStudentRepository _studentRepository;
+
+        public DeleteStudentHandler(IStudentRepository studentRepository)
+        {
+            _studentRepository = studentRepository;
+        }
+
+        public async Task<int> Handle(CQRSAndMediatRDemo.Commands.DeleteStudentCommand command, CancellationToken cancellationToken)
+        {
+            var studentDetails = await _studentRepository.GetStudentByIdAsync(command.Id);
+            if (studentDetails == null)
+                return default;
+
+            return await _studentRepository.DeleteStudentAsync(studentDetails.Id);
+        }
+    }
+}
